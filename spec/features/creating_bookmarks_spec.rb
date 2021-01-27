5d4f2a2a0d6bae6322288251e4f9f35ff52061c0
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-27 08:59:58 +0000

    refactored route to avoid new bookmarks/new and for index.erb to do the post request of adding bookmark

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -1,9 +1,18 @@
+# old feature test for bookmarks/new refactored below to all be done on index.erb 
+# feature 'Adding a new bookmark' do
+#   scenario 'A user can add a bookmark to Bookmark Manager' do
+#     visit('/bookmarks/new')
+#     fill_in('url', with: 'http://www.msn.com')
+#     click_button('Save')
+#     expect(page).to have_content 'http://www.msn.com'
+#   end
+# end
+
 feature 'Adding a new bookmark' do
   scenario 'A user can add a bookmark to Bookmark Manager' do
-    visit('/bookmarks/new')
+    visit('/bookmarks')
     fill_in('url', with: 'http://www.msn.com')
-    click_button('Submit')
-
+    click_button('Save')
     expect(page).to have_content 'http://www.msn.com'
   end
 end
